b8251692037277db95e1db5b7291319fea0c7527
bsaltmiras <bensaltmiras@gmail.com>
2017-04-04 12:53:49 -0500

    Added native maps and fab that could be used to filter event types.

--- components/Map.js
+++ components/Map.js
@@ -2,14 +2,18 @@ import React from 'react';
 import {
   Image,
 } from 'react-native'
+import { Components } from 'expo'
 import { View, Container, Header, Form, Title, Content, ListItem, Left, Button, Body, Right, Card, CardItem, Item, Input, Row, Switch, Radio, Grid, Text, Icon, Badge, Thumbnail } from 'native-base';
 
 export default class Map extends React.Component {
   render() {
     return (
-      <View>
-        <Text>TODO Map</Text>
-      </View>
+      <Components.MapView style={{flex:10,flexGrow:10, width:null,height:null}} />
     )
+    // return (
+    //   <View>
+    //     <Text>TODO Map</Text>
+    //   </View>
+    // )
   }
 }
--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -3,14 +3,17 @@ import {
   Image,
   Platform,
 } from 'react-native'
-import {
-  FontAwesome,
-} from '@expo/vector-icons';
-import { Container, Header, Title, Content, ListItem, Left, Button, Body, Right, Card, CardItem, Item, Input, Row, Switch, Radio, Grid, Text, Icon, Badge, Thumbnail } from 'native-base';
+import { View, Container, Header, Title, Content, ListItem, Left, Button, Fab, Body, Right, Card, CardItem, Item, Input, Row, Switch, Radio, Grid, Text, Icon, Badge, Thumbnail } from 'native-base';
 
 import Map from '../components/Map'
 
 export default class MapScreen extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isFabActive: false,
+    }
+  }
   render() {
     return (
       <Container>
@@ -31,17 +34,65 @@ export default class MapScreen extends React.Component {
           </Right>
         </Header>
 
-        <Button rounded
-            style={{position: 'absolute', left:20,bottom: (Platform.OS === 'ios') ? 20 : 40,padding:15,margin:0,width:45,height:45,zIndex:400,justifyContent: 'center'}}
-            onPress={() => this.props.navigator.push('newEvent')}
-          >
-          <Icon name="add" style={{margin: 0}} />
-        </Button>
+        <View style={{flex:1}}>
+          <View style={{position:'absolute',left:0,top:0,right:0,bottom:0}}>
+            <Map />
+          </View>
+        </View>
 
-        <Content>
-          <Map />
-        </Content>
+        <View>
+          <Fab
+              active={this.state.isFabActive}
+              direction="up"
+              style={{ backgroundColor: '#5067FF', flexWrap:'nowrap'}}
+              position="bottomLeft"
+              onPress={() => this.setState({isFabActive: !this.state.isFabActive})}
+          >
+              <Icon name="plus" />
+              {this.renderFabButtonRow('#FFF', '#252932', 'icon', 'icon')}
+              {this.renderFabButtonRow('#FFF', '#d9534f', 'icon', 'icon')}
+              {this.renderFabButtonRow('#FFF', '#5cb85c', 'icon', 'icon')}
+              {this.renderFabButtonRow('#FFF', '#62B1F6', 'icon', 'icon')}
+              {this.renderFabButtonRow('#FF9501', 'rgba(255,255,255,.9)', 'food', 'Food')}
+              {this.renderFabButtonRow('#FFF', '#2874F0', 'add', 'New Event')}
+          </Fab>
+      </View>
       </Container>
+    )
+  }
+
+  renderFabButtonRow(outlineColor, fillColor, icon, text) {
+    return (
+      <View style={{pflexDirection:'row', justifyContent:'space-between',  borderRadius:null,height:null,width:null, backgroundColor:'transparent'}}>
+        {this.renderFabButton(outlineColor, fillColor, icon)}
+        <Text style={{color:null, flexGrow:1, marginLeft:10}}>{text}</Text>
+      </View>
+    )
+  }
+
+  renderFabButton(outlineColor, fillColor, icon) {
+    const outerDiameter=32;
+    const innerDiameter=28;
+    const fontSize=20;
+    const shadow={
+      shadowColor: '#000000',
+      shadowOffset: {
+        width: 0,
+        height: 0
+      },
+      shadowRadius: 1,
+      shadowOpacity: .5
+    }
+    return (
+      <Button style={{backgroundColor: outlineColor, width:outerDiameter, height:outerDiameter, borderRadius:outerDiameter/2, ...shadow,
+        alignItems:'center',padding:null,justifyContent:'center'}}>
+        <Content style={{backgroundColor: fillColor,
+            flex:0,flexGrow:0,width:innerDiameter,height:innerDiameter,borderRadius:innerDiameter/2,
+            alignItems:'center',padding:null,justifyContent:'center'}}
+        >
+          <Icon name={icon} style={{color:outlineColor,fontSize:fontSize}} />
+        </Content>
+      </Button>
     )
   }
 }
